67afe935ed22bd7c5a0acd7a552ceacd41ecf32a
Felipe Sobrinho <felipe.sobrinho.3@outlook.com>
2025-09-09 10:17:21 -0300

    fix: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,23 +5,19 @@ export async function middleware(request: NextRequest) {
   const sessionCookie = getSessionCookie(request);
   const { pathname } = request.nextUrl;
 
-  if (pathname === "/dashboard") {
-    if (!sessionCookie) {
-      const loginUrl = new URL("/login", request.url);
-      return NextResponse.redirect(loginUrl);
-    }
+  if (pathname.startsWith("/dashboard") && !sessionCookie) {
+    const loginUrl = new URL("/login", request.url);
+    return NextResponse.redirect(loginUrl);
   }
 
-  if (sessionCookie) {
-    if (pathname === "/login" || pathname === "/cadastro") {
-      const dashboardUrl = new URL("/dashboard", request.url);
-      return NextResponse.redirect(dashboardUrl);
-    }
+  if (sessionCookie && (pathname === "/login" || pathname === "/cadastro")) {
+    const dashboardUrl = new URL("/dashboard", request.url);
+    return NextResponse.redirect(dashboardUrl);
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*"],
+  matcher: ["/dashboard/:path*", "/login", "/cadastro"],
 };
